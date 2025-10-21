da427f4aacac32b183c39a9627e0dfb85bec1662
Evolitist <evolitist@gmail.com>
2025-10-21 22:46:30 +0700

    update code formatting & app styling

--- app/src/main/java/com/evolitist/quicknote/presentation/list/ListScreen.kt
+++ app/src/main/java/com/evolitist/quicknote/presentation/list/ListScreen.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -21,8 +22,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStoreFile
@@ -34,18 +39,19 @@ private val PrefsDataKey = stringSetPreferencesKey("data")
 
 @Composable
 fun ListScreen(modifier: Modifier = Modifier) {
-    val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val prefs = remember {
         PreferenceDataStoreFactory.create {
             context.preferencesDataStoreFile("prefs")
         }
     }
+
+    var text by remember { mutableStateOf("") }
     val textList by prefs.data
         .map { it[PrefsDataKey]?.toList() ?: emptyList() }
         .collectAsState(emptyList())
-    var text by remember { mutableStateOf("") }
 
+    val scope = rememberCoroutineScope()
     fun saveValue() {
         val keyedText = "" + System.currentTimeMillis() + "|$text"
         scope.launch {
@@ -58,23 +64,27 @@ fun ListScreen(modifier: Modifier = Modifier) {
         text = ""
     }
 
-    Scaffold { innerPadding ->
+    Scaffold(
+        modifier = modifier,
+    ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp),
         ) {
             TextField(
                 value = text,
                 onValueChange = { text = it },
-                keyboardActions = KeyboardActions {
-                    saveValue()
-                },
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Sentences,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions { saveValue() },
                 trailingIcon = {
-                    Button(
-                        onClick = {
-                            saveValue()
-                        },
+                    TextButton(
+                        onClick = { saveValue() },
                         content = { Text("Save") },
-                        colors = ButtonDefaults.textButtonColors()
                     )
                 },
                 modifier = Modifier.fillMaxWidth(),
